a88bf7099bc6847dabdf5757b37c52584dc94176
Retroville <warhodes@gmail.com>
2021-03-02 17:44:45 -0500

    enhanced the price code to play well with events. also added buttons.

--- main.js
+++ main.js
@@ -19,12 +19,15 @@ function hireAdventurer(n) {
     document.getElementById('adventurerCost').innerHTML = nextCost;
 }
 
+
 var scrolls = 0;
-var scrollPrice = 55;
-var scrollBias = 0;
-var scrollVol = 2;
-var scrollMax = 300;
-var scrollMin = 13;
+var scrollPrice = 10;
+var scrollBias = 20;
+var scrollVol = 10;
+//scrollVolume? buy too much and tank the price
+
+document.getElementById('scrollBias').innerHTML = scrollBias;
+document.getElementById('scrollVol').innerHTML = scrollVol;
 
 function b_up() {scrollBias = scrollBias + 1; document.getElementById('scrollBias').innerHTML = scrollBias;}
 function b_down() {scrollBias = scrollBias - 1; document.getElementById('scrollBias').innerHTML = scrollBias;}
@@ -33,41 +36,64 @@ function v_down() {scrollVol = scrollVol - 1; document.getElementById('scrollVol
 
 
 function buyScroll(n) {
-    if (gold >= scrollPrice) {
-        scrolls = scrolls + 1;
-        gold = Number((gold - scrollPrice).toFixed(2));
+    if (gold >= scrollPrice * n) {
+        scrolls = scrolls + n;
+        gold = Number((gold - scrollPrice * n).toFixed(2));
         document.getElementById('gold').innerHTML = gold;
         document.getElementById('scrolls').innerHTML = scrolls;
     }
 }
 
 function sellScroll(n) {
-    if (scrolls > 0) {
-        scrolls = scrolls - 1;
-        gold = Number((gold + scrollPrice).toFixed(2));
+    if (scrolls >= n) {
+        scrolls = scrolls - n;
+        gold = Number((gold + scrollPrice * n).toFixed(2));
         document.getElementById('gold').innerHTML = gold;
         document.getElementById('scrolls').innerHTML = scrolls;
     }
 }
 
-function priceShift(bias, vol) {
-    var rv = 1 + ((Math.random() * vol) - vol/2 + bias)/100;
-    console.log("rv: " + rv);
-    return rv;
+//-----------------------------------------------------------------------------
+
+function priceShift(bias, vol, price) {
+    // returns the value of daily movement. E.g. -$15 for the day
+    // Bias is the 'target' price
+    // Vol is the range of prices, stored as absolute value of offset from bias
+    // The price can not move more than range/4 (vol/2) per day
+    // if the price is within 1 maxMove of ceiling or floor, the price will
+    //      either stay the same, or it will move opposite of ceil/floor
+
+    var maxMove = Math.floor(vol /2); //dont move more than vol/2
+    var move = Math.floor((Math.random() * maxMove + 1));
+
+    if (price >= bias+vol) { //if price > ceiling, it must trend down
+        return 0 - move;
+    } else if (price <= bias-vol) { // if price < floor, it must trend up
+        return move;
+    } else { //otherwise just 50/50 up or down
+        if (Math.floor(Math.random() * 2) == 0) {
+            return move;
+        } else {
+            return 0 - move;
+        }
+    }
 }
 
-var hour = 0;
-var die = 0;
+var time = 0;
 
 window.setInterval(function() {
-    hour = hour + 1;
+    time = time + 1;
     basicClick(adventurers);
 
-    scrollPrice = scrollPrice * priceShift(scrollBias,scrollVol);
+    if (time % 24 == 0) {
+        scrollPrice = scrollPrice + priceShift(scrollBias, scrollVol, scrollPrice);
+    }
+    if (time % 6 == 0) {
+        scrollPrice = scrollPrice + priceShift(scrollBias, scrollVol, scrollPrice);
+    }
 
-    document.getElementById('scrollHigh').innerHTML = scrollBias + scrollVol;
-    document.getElementById('scrollLow').innerHTML = scrollBias - scrollVol;
     document.getElementById('scrollPrice').innerHTML = scrollPrice.toFixed(2);
-    document.getElementById('tradingDay').innerHTML = Math.floor(hour/12) + 1;
-    document.getElementById('hour').innerHTML = hour;
+    document.getElementById('tradingDay').innerHTML = Math.floor(time/24) + 1;
+    document.getElementById('tradingHour').innerHTML = Math.floor(time % 24);
+    document.getElementById('hour').innerHTML = time;
 }, 500);
