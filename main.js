c42ca2c65434eef2239c9197dc78edf1eee89598
Andrew-Banks-UT <andrewb723@hotmail.com>
2021-03-18 17:21:10 -0400

    Uptaded events

--- main.js
+++ main.js
@@ -104,33 +104,44 @@ function ev(item1, item2, title, text, Vol1, Vol2, jump1, jump2){
   this.Vol2=Vol2;
   this.title=title;
   this.text=text;
-  this.jump2=jump1;
+  this.jump1=jump1;
   this.jump2=jump2
 }
 var randEv = new ev();
 var events = [];
-function create_events(events){
-  //var events = [];
-  alert("Events loaded!");
-  create = new ev(swords, blanks, "Axes in Fashion",
-  "After a recent raid by some dashing vikings, people have become smitten with axes."
-  , 1, 0, -20, 0);
-  events.push(create);
+var kings = [];
+function king(name, description, typeUp, typeDown){
+  this.name = name;
+  this.description = description;
+  this.typeUp = typeUp;
+  this.typeDown = typeDown;
 }
+
+
 function get_event(randEv){
-  var randEv
+  var randEv = new ev();
   randEv=events[Math.floor(Math.random() * events.length)];
-  alert(randEv.title);
+  //alert(randEv.title);
   if(randEv.item1.name != "blanks"){
     randEv.item1.vol += randEv.Vol1;
     randEv.item1.bias += randEv.jump1;
+    //randEv.item1.price +=randEv.jump1;
   }
   if(randEv.item2.name != "blanks"){
     randEv.item2.vol += randEv.Vol2;
     randEv.item2.bias += randEv.jump2;
   }
 }
+function create_events(){
+  //var events = [];
+  alert("Events loaded!");
+  create = new ev(swords, blanks, "Axes in Fashion",
+  "After a recent raid by some dashing vikings, people have become smitten with axes."
+  , 10, 0, -20, 0);
+  events.push(create);
+}
 window.setInterval(function() {
+    var randEv = new ev()
     // This function will run every gamespeed ms. Generally updates the price of
     // items (as well as the player's money from 'clicker' stuff) and then
     // draws this to the screen.
@@ -149,11 +160,13 @@ window.setInterval(function() {
             item.price = item.price + priceShift(item.bias, item.vol, item.price);
         }
     }
-    if (time % 12 != 0){
+    if (time % 36 == 0){
       get_event(randEv);
     }
     for (item of allItems) {
         document.getElementById(item.name + "Price").innerHTML = item.price.toFixed(2);
+        document.getElementById(item.name + "Bias").innerHTML = item.bias.toFixed(2);
+        document.getElementById(item.name + "Vol").innerHTML = item.vol.toFixed(2);
     }
 
     document.getElementById("tradingDay").innerHTML = Math.floor(time/24) + 1;
