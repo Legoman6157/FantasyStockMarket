0c7ae4d417b6ba378b1c579edcdb92ee9d64cad8
Andrew-Banks-UT <andrewb723@hotmail.com>
2021-04-01 16:11:20 -0400

    Refined King system. Issues with allItems for loop

--- main.js
+++ main.js
@@ -41,6 +41,7 @@ var swords = new Item("sword", 80, 80, 20);
 var axes = new Item("axe", 50, 50, 30);
 var shields = new Item("shield", 30, 30, 10);
 var grain = new Item("grain", 15, 12, 5);
+var leather = new Item("leather", 12, 12, 5);
 var livestock = new Item("livestock", 17, 20, 20);
 var wine = new Item("wine", 100, 90, 25);
 var crystals = new Item("crystal", 90, 90, 20);
@@ -52,18 +53,22 @@ var goblin_hide = new Item("goblin hide", 10, 10, 10);
 var rum = new Item("rum", 20, 30, 10);
 var cyclops_eyes = new Item("cyclops eye", 80, 80, 20);
 var unicorn_horn = new Item("unicorn horn", 120, 110, 30);
-var pheonix_ash = new Item("sword", 200, 200, 50);
+var phoenix_ash = new Item("phoenix ash", 200, 200, 50);
 var blanks =new Item("blanks", 0, 0, 0);
-var allItems = [scrolls, swords, axes, shields, grain, livestock
-, wine, crystals, dragon_scales, scepters, tomes, troll_teeth
-, goblin_hide, rum, cyclops_eyes, unicorn_horn, pheonix_ash,];
-
+var allItems = [scrolls, swords, axes, shields, grain, leather, livestock,
+ wine, crystals, dragon_scales, scepters, tomes, troll_teeth,
+ goblin_hide, rum, cyclops_eyes, unicorn_horn, phoenix_ash];
+var war =[swords, axes, shields];
+var magic = [scrolls, crystals, scepters, tomes];
+var goods = [grain, livestock, leather, wine, rum];
+var monster_parts = [dragon_scales, troll_teeth, cyclops_eyes,
+goblin_hide, unicorn_horn, phoenix_ash];
 
 //debug functions to control bias/volatility
-for (item of allItems) {
-    document.getElementById(item.name + "Bias").innerHTML = item.bias; //lol
-    document.getElementById(item.name + "Vol").innerHTML = item.vol;
-}
+//for (item of allItems) {
+  //  document.getElementById(item.name + "Bias").innerHTML = item.bias; //lol
+  //  document.getElementById(item.name + "Vol").innerHTML = item.vol;
+//}
 
 function b_up(item) {item.bias = item.bias + 1; document.getElementById(item.name + "Bias").innerHTML = item.bias;}
 function b_down(item) {item.bias = item.bias - 1; document.getElementById(item.name + "Bias").innerHTML = item.bias;}
@@ -105,7 +110,7 @@ function sellItem(n, item) {
         document.getElementById("gold").innerHTML = gold;
         document.getElementById(item.name + "Quantity").innerHTML = item.shares.length();
         if (item.shares.length > 0) {
-            document.getElementById(item.name + "AverageCost").innerHTML = (item.total_cost / item.shares.length).toFixed(2)
+            document.getElementById(item.name + "AverageCost").innerHTML = (item.total_cost / item.shares.length).toFixed(2);
         } else {
             document.getElementById(item.name + "AverageCost").innerHTML = 0;
         }
@@ -125,8 +130,6 @@ var randEv = new ev();
 var events = [];
 var kings = [];
 var candidates =[];
-var currKing = new king("King Neutralitus III",
-"A king who cares little for the kingdoms economic affairs.", null, null);
 
 function king(name, description, typeUp, typeDown){
   this.name = name;
@@ -134,19 +137,58 @@ function king(name, description, typeUp, typeDown){
   this.typeUp = typeUp;
   this.typeDown = typeDown;
 }
+var currKing = new king("King Neutralitus III",
+"A king who cares little for the kingdoms economic affairs.", null, null);
 
 function create_kings(){
   //var events = [];
   alert("Kings loaded!");
-  create = new king("Queen Audra II",
+  k1 = new king("Queen Audra II",
   "A sorcerrer's apprentence", magic, war);
-  kings.push(create);
+  k2 = new king("King Bartholemau I",
+  "A military general who believes the neibouring kindom will declar war", war, goods)
+  kings.push(k1);
+  kings.push(k2);
+}
+
+function pick_candidates(){
+  var rand;
+  var candidate;
+  for(var i=0; i<2; i++){
+    rand = Math.floor(Math.random() * kings.length);
+    candidate = kings[rand];
+    candidates.push(candidate);
+    kings.splice(rand, 1);
+  }
+}
+
+function succession(){
+  var i;
+  currKing= candidates[[Math.floor(Math.random() * candidates.length)]];
+  for(i=0; i<currKing.typeUp.size; i++){
+    currKing.typeUp[i].price+=10;
+    currKing.typeUp[i].bias+=20;
+  }
+  for(i=0; i<currKing.typeDown.size; i++){
+    if(currKing.typeDown[i].price-10>1){
+      currKing.typeDown[i].price-=10;
+    }
+    else{
+      currKing.typeDown[i].price=1
+    }
+    if(currKing.typeDown[i].bias-20<10){
+      currKing.typeDown[i].price=10
+    }
+    else{
+      currKing.typeDown[i].bias-=20;
+    }
+  }
 }
 
 function get_event(randEv){
   var randEv = new ev();
   randEv=events[Math.floor(Math.random() * events.length)];
-  //alert(randEv.title);
+  alert(randEv.title);
   if(randEv.item1.name != "blanks"){
     if(randEv.item1.vol + randEv.Vol1<1){
       randEv.item1.vol = 1;
@@ -168,12 +210,45 @@ function get_event(randEv){
   }
 }
 function create_events(){
-  //var events = [];
+  var events = [];
   alert("Events loaded!");
   create = new ev(swords, blanks, "Axes in Fashion",
   "After a recent raid by some dashing vikings, people have become smitten with axes."
   , 10, 0, -20, 0);
+  ev1 = new ev(swords, axes, "War declared!", "A neighboring kingdom has declared war on our noble nation, causing many to buy weapons.", 10, 10, 20, 20);
+  ev2 = new ev(swords, shields, "Invasion!", "Our kingdom has been invaded! Invest in swords and shields to protect yourself!", 5, 5, 15, 20);
+  ev4 = new ev(grain, blanks, "Drought", "Our land has not received rainfall since the last full moon, causing the crops to grow less.", 20, 0, -25, 0);
+  ev5 = new ev(crystals, blanks, "Eureka!", "Explorers have discovered a foreign land trading thousands of crystals!", 15, 0, 15, 0);
+  ev6 = new ev(crystals, blanks, "Tariff", "Our traders have placed a tariff on exported crystals, causing less of them to be shipped to our nation.", 15, 0, -15, 0);
+  ev7 = new ev(dragon_scales, blanks, "Save the dragons!", "Activists have insisted that people stop killing dragons for their scales.", 5, 0, -5, 0);
+  ev8 = new ev(unicorn_horn, blanks, "Save the unicorns!", "Activists have insisted that people stop killing unicorns for their horns.", 5, 0, -5, 0);
+  ev9 = new ev(tomes, blanks, "Tomes in fashion!", "I'm not quite sure what a tome is, but a lot of people seem to be buying them right now! Invest!", 10, 0, 10, 0);
+  ev10 = new ev(cyclops_eyes, blanks, "Cyclops island discovered!", "An island inhabited by cyclopses has been discovered and pillaged, leading to a surplus in cyclops eyes.", 10, 0, 5, 0);
+  ev11 = new ev(leather, blanks, "Leather in fashion!", "Many people are buying leather as a new fashion trend.", 10, 0, 10, 0);
+  ev12 = new ev(leather, blanks, "Leather out of fashion!", "People have seemed to lose interest in leather. Expect stocks to fall.", 10, 0, -5, 0);
+  //ev13 = new ev(pegasus_feathers, blanks, "Feathers in fashion!", "People have begun wearing pegasus feathers in their hair as a good luck charm! Invest!", 8, 0, 6, 0);
+  ev14 = new ev(scepters, blanks, "A surplus in scepters!", "There seems to be a huge interest in scepters recently! Expect stocks to raise!", 5, 0, 7, 0);
+  ev15 = new ev(phoenix_ash, blanks, "Ash everywhere!", "It is phoenix season, and their ash is going in fashion!", 7, 0, 27, 0);
+  ev16 = new ev(troll_teeth, blanks, "Trolled!", "Troll teeth are going out of fashion.", 5, 0, -10, 0);
+  ev17 = new ev(goblin_hide, blanks, "Hides in fashion!", "People have become increasingly impressed with goblin hides, causing prices to increase!", 5, 0, 3, 0);
   events.push(create);
+  events.push(ev1);
+  events.push(ev2);
+//  events.push(ev3);
+  events.push(ev4);
+  events.push(ev5);
+  events.push(ev6);
+  events.push(ev7);
+  events.push(ev8);
+  events.push(ev9);
+  events.push(ev10);
+  events.push(ev11);
+  events.push(ev12);
+  //events.push(ev13);
+  events.push(ev14);
+  events.push(ev15);
+  events.push(ev16);
+  events.push(ev17);
 }
 
 // Game loop and price modulation-----------------------------------------------
